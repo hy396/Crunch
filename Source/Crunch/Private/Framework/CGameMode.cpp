3a2c54ccdc140cc399bbf6336d06c5afc56c69ed
HuanYu <3291587145@qq.com>
2025-07-01 17:30:31 +0800

    添加代码

---
+++ Source/Crunch/Private/Framework/CGameMode.cpp
@@ -0,0 +1,5 @@
+// 幻雨喜欢小猫咪
+
+
+#include "Crunch/Private/Framework/CGameMode.h"
+
---
+++ Source/Crunch/Private/Framework/CGameMode.h
@@ -0,0 +1,17 @@
+// 幻雨喜欢小猫咪
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
